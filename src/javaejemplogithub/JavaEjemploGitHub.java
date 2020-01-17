37f38586a4981d02f89b380534be30b227529187
sergi <sergi@PC_Balduino>
2020-01-17 13:01:04 +0100

    tercera modificacion

--- src/javaejemplogithub/JavaEjemploGitHub.java
+++ src/javaejemplogithub/JavaEjemploGitHub.java
@@ -14,16 +14,29 @@ import java.util.Scanner;
 public class JavaEjemploGitHub {
 
     /**
+     * Esta funcion suma tres numeros
+     * @param n1 primer numero
+     * @param n2 segundo numero
+     * @param n3 tercer numero
+     * @return devuelve la suma de los tres numeros
+     */
+    
+    public int sumar3numeros(int n1, int n2, int n3){
+        int suma = n1 + n2 + n3;
+        return suma;
+    }
+    
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Dime un numero");
-       int numero = teclado.nextInt();
-       
+        int numero = teclado.nextInt();
+
         for (int i = 0; i < 10; i++) {
             System.out.println(numero * i);
         }
     }
-    
 }
